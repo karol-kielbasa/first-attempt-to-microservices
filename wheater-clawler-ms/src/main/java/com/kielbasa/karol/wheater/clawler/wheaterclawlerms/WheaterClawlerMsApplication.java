098cd397c2fc6bd1eec3488297d70460b5d0c1ae
KAROL\Kiełbasa Karol <karsen1995@gmail.com>
2019-01-02 19:00:57 +0100

    added new ms template

---
+++ wheater-clawler-ms/src/main/java/com/kielbasa/karol/wheater/clawler/wheaterclawlerms/WheaterClawlerMsApplication.java
@@ -0,0 +1,14 @@
+package com.kielbasa.karol.wheater.clawler.wheaterclawlerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheaterClawlerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheaterClawlerMsApplication.class, args);
+	}
+
+}
+
---
+++ wheater-clawler-ms/src/test/java/com/kielbasa/karol/wheater/clawler/wheaterclawlerms/WheaterClawlerMsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kielbasa.karol.wheater.clawler.wheaterclawlerms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WheaterClawlerMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
